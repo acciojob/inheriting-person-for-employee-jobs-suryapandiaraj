5aa857fde5c3e8e51265c2189efafe52016bcd92
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2024-10-12 12:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+funtion greet(){
+	return `Hello, my name is ${this.name}, I am ${this.age} years old.`
+
+}
+
+function Employee(name, age, jobTitle) {
+	Person.call(name, age);
+	this.jobTitle = jobTitle;
+}
+
+function jobGreet(){
+		return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${jobTitle}.`
+
+}
 
 // Do not change code below this line
 window.Person = Person;
